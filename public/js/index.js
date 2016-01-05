4f37155a0d6a3ff1302330eeb3d5e07d98f27e7d
Keith Wedinger <keith.wedinger@leadingedje.com>
2016-01-05 15:51:39 -0500

    Finished styling to match instructions

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 var socket = io();
 $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
+    socket.emit('chat message', $('#message-box').val());
+    $('#message-box').val('');
     return false;
 });
 socket.on('chat message', function(msg){
