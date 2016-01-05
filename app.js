f4ad60d7ddc96a754b3da318b4ef015495606fc8
Keith Wedinger <keith.wedinger@leadingedje.com>
2016-01-05 13:54:02 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Use port 3000 if PORT env var is not defined
+var port = process.env.PORT || 3000;
+
+/**
+ * Route to index.html
+ */
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, function () {
+    console.log('listening on ' + port);
+});
